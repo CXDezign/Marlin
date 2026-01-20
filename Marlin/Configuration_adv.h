6914ea31783ade1cf3744b3aa7738168c26e11e6
CXDezign <account@cxdezign.dev>
2026-01-20 23:50:51 +0100

    Configuration_adv.h - Preset Functions - M999 Motherboard Restart

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4063,30 +4063,34 @@
   #define CUSTOM_MENU_MAIN_SCRIPT_AUDIBLE_FEEDBACK
   //#define CUSTOM_MENU_MAIN_SCRIPT_RETURN   // Return to status screen after a script
   //#define CUSTOM_MENU_MAIN_ONLY_IDLE         // Only show custom menu when the machine is idle
+  #define MAIN_MENU_ITEM_1_DESC "Restart Motherboard"
 
-  #define MAIN_MENU_ITEM_1_DESC "Home & ABL"
-  #define MAIN_MENU_ITEM_1_GCODE "G28\nG29"
+  #define MAIN_MENU_ITEM_1_GCODE "M997"
   #define MAIN_MENU_ITEM_1_CONFIRM          // Show a confirmation dialog before this action
 
-  // #define MAIN_MENU_ITEM_2_DESC "Preheat for " PREHEAT_1_LABEL
-  // #define MAIN_MENU_ITEM_2_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
-  // #define MAIN_MENU_ITEM_2_CONFIRM
+  #define MAIN_MENU_ITEM_2_DESC "Home & ABL"
+  #define MAIN_MENU_ITEM_2_GCODE "G28\nG29"
+  #define MAIN_MENU_ITEM_2_CONFIRM          // Show a confirmation dialog before this action
 
-  // #define MAIN_MENU_ITEM_2_DESC "Preheat for " PREHEAT_1_LABEL
-  // #define MAIN_MENU_ITEM_2_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
-  //#define MAIN_MENU_ITEM_2_CONFIRM
+  // #define MAIN_MENU_ITEM_3_DESC "Preheat for " PREHEAT_1_LABEL
+  // #define MAIN_MENU_ITEM_3_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
+  // #define MAIN_MENU_ITEM_3_CONFIRM
 
-  //#define MAIN_MENU_ITEM_3_DESC "Preheat for " PREHEAT_2_LABEL
-  //#define MAIN_MENU_ITEM_3_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
-  //#define MAIN_MENU_ITEM_3_CONFIRM
-
-  //#define MAIN_MENU_ITEM_4_DESC "Heat Bed/Home/Level"
-  //#define MAIN_MENU_ITEM_4_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nG28\nG29"
+  // #define MAIN_MENU_ITEM_4_DESC "Preheat for " PREHEAT_1_LABEL
+  // #define MAIN_MENU_ITEM_4_GCODE "M140 S" STRINGIFY(PREHEAT_1_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_1_TEMP_HOTEND)
   //#define MAIN_MENU_ITEM_4_CONFIRM
 
-  //#define MAIN_MENU_ITEM_5_DESC "Home & Info"
-  //#define MAIN_MENU_ITEM_5_GCODE "G28\nM503"
+  //#define MAIN_MENU_ITEM_5_DESC "Preheat for " PREHEAT_2_LABEL
+  //#define MAIN_MENU_ITEM_5_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nM104 S" STRINGIFY(PREHEAT_2_TEMP_HOTEND)
   //#define MAIN_MENU_ITEM_5_CONFIRM
+
+  //#define MAIN_MENU_ITEM_6_DESC "Heat Bed/Home/Level"
+  //#define MAIN_MENU_ITEM_6_GCODE "M140 S" STRINGIFY(PREHEAT_2_TEMP_BED) "\nG28\nG29"
+  //#define MAIN_MENU_ITEM_6_CONFIRM
+
+  //#define MAIN_MENU_ITEM_7_DESC "Home & Info"
+  //#define MAIN_MENU_ITEM_7_GCODE "G28\nM503"
+  //#define MAIN_MENU_ITEM_7_CONFIRM
 #endif
 
 // @section custom config menu
