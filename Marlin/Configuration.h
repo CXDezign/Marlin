38d3fd40e881765f14bc88268929ed903f4a9de9
CXDezign <account@cxdezign.dev>
2026-01-27 19:05:41 +0100

    Configuration.h - Update default max acceleration and bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1339,7 +1339,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1904,7 +1904,7 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 218
+#define X_BED_SIZE 220
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
